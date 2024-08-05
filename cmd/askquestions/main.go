1da5e08a538895e7561add8249ece494dff07d9e
Pedro Gomes <pcgbernardes@gmail.com>
2024-08-05 11:11:50 -0400

    feat: add api package and main function

---
+++ cmd/askquestions/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/pe-Gomes/askme-realtime/internal/api"
+	"github.com/pe-Gomes/askme-realtime/internal/store/pgstore"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(fmt.Errorf("error loading env files: %w", err))
+	}
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_PORT"),
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_NAME"),
+	))
+	if err != nil {
+		panic(fmt.Errorf("error creating pool to database: %w", err))
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(fmt.Errorf("error connection to database: %w", err))
+	}
+
+	handler := api.NewHandler(pgstore.New(pool))
+
+	go func() {
+		if err := http.ListenAndServe(":8080", handler); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				panic(fmt.Errorf("error starting server: %w", err))
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	<-quit
+}
